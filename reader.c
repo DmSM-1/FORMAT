189b3725b43bd4faf6f1d3bb8d5d59b892bc8a74
DmSM-1 <69243181+DmSM-1@users.noreply.github.com>
2023-02-12 15:42:18 +0300

    Update reader.c

--- reader.c
+++ reader.c
@@ -1,5 +1,8 @@
 #include <sndfile.h>
-#define PATH "15.wav" 
+#include <stdint.h>
+#include <stdlib.h>
+#define PATHIN "output2.wav" 
+#define PATHOUT "2.wav"
 
 void sfInfoPrint(SF_INFO* sfinfo){
 
@@ -18,8 +21,25 @@ void sfInfoPrint(SF_INFO* sfinfo){
 }
 
 void main(){
-    SF_INFO sfinfo;
-    SNDFILE* file = sf_open(PATH, SFM_READ, &sfinfo);
-    sfInfoPrint(&sfinfo);
-    sf_close(file);
+    SF_INFO sfinfoIN;
+    SNDFILE* fileIN = sf_open(PATHIN, SFM_READ, &sfinfoIN);
+    FILE* fp = fopen("data.txt", "w");
+    //SF_INFO sfinfoOUT = {sfinfoIN.frames, sfinfoIN.samplerate, sfinfoIN.channels, sfinfoIN.format, sfinfoIN.sections, sfinfoIN.seekable};
+    //SNDFILE* fileOUT = sf_open(PATHOUT, SFM_WRITE, &sfinfoOUT);
+
+    double buffer[1024];
+    int count = 0;
+    
+    //printf("%d\n", sf_read_double(fileIN, &buffer, 1));
+    for(int i = 0;(count = sf_read_double(fileIN, buffer, 1024));){
+        for (int j = 0; j < count; j++){
+            i++;
+            fprintf(fp, "%d %lf\n",i, buffer[j]);
+        }
+        //printf("%d, %lf\n",reader,  buffer[0]);
+    }
+
+    fclose(fp);
+    sf_close(fileIN);
+    //sf_close(fileOUT);
 }
